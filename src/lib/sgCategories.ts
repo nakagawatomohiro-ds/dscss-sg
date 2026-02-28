ebefdc6a2186ae8885b147eba3c61c5bde499632
nakagawatomohiro-ds <nakagawa.tomohiro@dropstone.co.jp>
2026-02-28 16:48:37 +0900

    Add seedData0 import to sgCategories for 270 total questions

--- src/lib/sgCategories.ts
+++ src/lib/sgCategories.ts
@@ -1,3 +1,4 @@
+import { seedData0 } from "./seedData0";
 import { seedData1a } from "./seedData1a";
 import { seedData1b } from "./seedData1b";
 import { seedData1c } from "./seedData1c";
@@ -11,4 +12,4 @@ import { seedData3b } from "./seedData3b";
 import { seedData3c } from "./seedData3c";
 import { seedData3d } from "./seedData3d";
 
-export const sgQuestions = [...seedData1a, ...seedData1b, ...seedData1c, ...seedData1d, ...seedData2a, ...seedData2b, ...seedData2c, ...seedData2d, ...seedData3a, ...seedData3b, ...seedData3c, ...seedData3d];
+export const sgQuestions = [...seedData0, ...seedData1a, ...seedData1b, ...seedData1c, ...seedData1d, ...seedData2a, ...seedData2b, ...seedData2c, ...seedData2d, ...seedData3a, ...seedData3b, ...seedData3c, ...seedData3d];
